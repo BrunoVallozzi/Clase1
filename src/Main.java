30325cea9529d9834128ed0ff8341d13a33b606a
Bruno Vallozzi <bruno.vallozzi@gmail.com>
2023-03-11 10:44:08 -0300

    Segundo commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        float ingMen = 489083;
+        int canVeh = 3;
+        int antVeh = 5;
+        int inm = 3;
+        boolean cuaReq = true;
+
+
+
+
+
+
+    }
+
+}
+
+
